831e20d63a48acc034641bf2e20d5d9b2da20e60
Erin Green <erin.y.green@gmail.com>
2018-07-17 15:18:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var otherDeli = [];
 
 function takeANumber(katzDeli, name) {
   katzDeli.push(name); 
-  return `Welcome ${name}, you are number ${katzDeli.length} in line.`;
+  return `Welcome ${name}. You are number ${katzDeli.length} in line.`;
 }
 
 function nowServing(katzDeli, name) {
