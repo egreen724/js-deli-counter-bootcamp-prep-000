8d0a34895906d7e8c89d17dd9796ad4f64500e1e
Erin Green <erin.y.green@gmail.com>
2018-07-17 14:58:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  return `Welcome ${name}, you are number X in line.`;
+}
+
+function nowServing(katzDeliLine, name) {
+  return `Currently serving ${name}.`
+  or "There is nobody waiting to be served!"
+}
+
+function currentLine(katzDeliLine, name) {
+  return "The line is currently: 1. katzDeliLine[0] 2. katzDeliLine[1] 3. katzDeliLine[2].";
+  or "The line is currently empty."
+}
